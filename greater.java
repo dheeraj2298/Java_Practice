adcdcc4cfccbfdcaf0e4285812ae041d3924f1db
dheeraj2298 <davkhardheeraj2230@gmail.com>
2021-06-13 18:39:21 +0530

    created java file greater.java

---
+++ greater.java
@@ -0,0 +1,30 @@
+/*
+accept two numbers and check whether first number is greater or
+second number is greater or they are equal.
+*/
+
+
+import java.util.*;
+public class Equal
+{
+	public static void main(String args[])
+	{
+		System.out.println("enter a number no1,no2");
+		Scanner sc=new Scanner(System.in);
+		int no1=sc.nextInt();
+		int no2=sc.nextInt();
+		if(no1 > no2)
+		{
+			System.out.println("first number is greater");
+		}
+		else if(no1 < no2)
+		{
+			System.out.println("second number is greater");
+		}
+		else
+		{
+			System.out.println("both number are equal");
+		}
+	}
+}
+
